7600d6b1386fcfc56b296ca28ef6ad9e93183bdc
OSCAR-WOS <theoscar@protonmail.com>
2020-07-27 09:41:42 +0100

    Fixed /tmp directory

--- events/message.js
+++ events/message.js
@@ -9,13 +9,13 @@ module.exports = (client, message) => {
 
 async function cacheAttachment(message, attachment) {
   try {
-      if (!fs.existsSync(`/tmp/${attachment.id}`)) fs.mkdirSync(`/tmp/${attachment.id}`);
+      if (!fs.existsSync(`./tmp/${attachment.id}`)) fs.mkdirSync(`./tmp/${attachment.id}`);
       let file = await fetch(attachment.url);
-      let dest = fs.createWriteStream(`/tmp/${attachment.id}/${attachment.name}`);
+      let dest = fs.createWriteStream(`./tmp/${attachment.id}/${attachment.name}`);
       await file.body.pipe(dest);
 
       let channel = message.guild.channels.cache.get(message.guild.db.log.files);
-      let sent = await channel.send('', { files: `/tmp/${attachment.id}/${attachment.name}`});
+      let sent = await channel.send('', { files: `./tmp/${attachment.id}/${attachment.name}`});
       attachment.link = sent;
   } catch { }
 }
