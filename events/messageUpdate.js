5a6d45d1b60db325f2a63bf51d6d64ec1cb7548d
unknown <theoscar@protonmail.com>
2020-07-29 21:46:17 +0100

    Fixed createdTimestamp of edited messages

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,6 +4,7 @@ module.exports = async (client, oldMessage, newMessage) => {
   if (oldMessage.cleanContent == newMessage.cleanContent) return;
   if (!newMessage.hasOwnProperty('changes')) newMessage.changes = [];
   newMessage.changes.push(oldMessage);
+  newMessage.createdTimestamp = new Date();
 
   try { log.send(newMessage.guild, { old: oldMessage, new: newMessage }, log.Type.MESSAGE_UPDATE); }
   catch { }
