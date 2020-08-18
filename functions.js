fc2a561074b298befb4f8796ea7e670afcf18459
unknown <theoscar@protonmail.com>
2020-08-18 21:35:44 +0100

    Fix typo

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-module.exports.logLengthCheck = functiong(string) {
+module.exports.logLengthCheck = function(string) {
   if (string.length < 500 && string.split('\n').length < 5) return true;
   return false;
 }
