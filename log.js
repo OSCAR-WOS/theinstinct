08d4230ebbdee7e7d6f4f8f3221694e9e18fd7d6
unknown <theoscar@protonmail.com>
2020-08-20 23:03:37 +0100

    another one...

--- log.js
+++ log.js
@@ -19,7 +19,7 @@ module.exports.Type = Type;
 module.exports.send = function(guild, data, type) {
   return new Promise(async (resolve, reject) => {
     if (!guild.hasOwnProperty('ready') || guild.db.logs.channel == null) return resolve();
-    if (!guild.db.log.enabledModules.includes(type)) return resolve();
+    if (!guild.db.enabledModules.includes(type)) return resolve();
 
     try {
       switch (type) {
