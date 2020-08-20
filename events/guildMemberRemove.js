fad0817cd70c530c6fa2fdf72407f4555644e79b
unknown <theoscar@protonmail.com>
2020-08-20 08:45:42 +0100

    added error checks

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -6,40 +6,33 @@ module.exports = async (client, guildMember) => {
   let log = null;
 
   if (guild.me.permissions.has('VIEW_AUDIT_LOG')) {
-    try { log = await checkAuditBan(guild, guildMember); }
+    try { log = await checkAudit(guild, guildMember, 'BAN'); }
     catch { }
 
     if (!log) {
-      try { log = await checkAuditKick(guild, guildMember); }
+      try { log = await checkAudit(guild, guildMember, 'KICK'); }
       catch { }
     }
   }
+
+  console.log('1');
 
   try {
     if (!log) return log.send(guild, guildMember, log.Type.LEAVE);
     if (log.executor && log.executor.bot) return;
 
-    console.log('check 1');
+    console.log('2');
     //if (log.action === 'MEMBER_BAN')
-  } catch { }
+  } catch (e) { console.error(e); }
 }
 
-function checkAuditBan(guild, guildMember) {
+function checkAudit(guild, guildMember, type) {
   return new Promise(async (resolve, reject) => {
     try {
-      let auditLog = await functions.fetchAuditLog(guild, 'MEMBER_BAN_ADD');
-      if (!auditLog) return resolve(null);
+      let auditLog;
 
-      if (auditLog.target.id != guildMember.id) return resolve(null);
-      return resolve(auditLog);
-    } catch { resolve(null); }
-  })
-}
-
-function checkAuditKick(guild, guildMember) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let auditLog = await functions.fetchAuditLog(guild, 'MEMBER_KICK');
+      if (type == 'BAN') auditLog = await functions.fetchAuditLog(guild, 'MEMBER_BAN_ADD');
+      else auditLog = await functions.fetchAuditLog(guild, 'MEMBER_KICK');
       if (!auditLog) return resolve(null);
 
       if (auditLog.target.id != guildMember.id) return resolve(null);
