a08f17e7c6defc6a788b3b5b5e671fe4277caaa7
unknown <theoscar@protonmail.com>
2020-08-20 13:56:58 +0100

    clear junk and moved delay to env

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -16,7 +16,6 @@ module.exports = (client, guild, user) => {
       catch (e) { console.error(e); }
     }
 
-    console.log(audit);
     if (!audit) return;
 
     try {
@@ -25,7 +24,7 @@ module.exports = (client, guild, user) => {
 
       log.send(guild, { member: member, executor: executor, reason: audit.reason }, log.Type.BAN);
     } catch { }
-  }, 500, guild, member)
+  }, process.env.delay, guild, member)
 }
 
 function checkAuditEntry(guild, member) {
@@ -34,24 +33,14 @@ function checkAuditEntry(guild, member) {
       let auditLog = await functions.fetchAuditLog(guild, 'MEMBER_BAN_ADD');
       if (!auditLog) return resolve(null);
 
-      console.log(`${auditLog.id} | ${guild.audit.ban.id}`);
-
-      console.log('1');
-
       let lastBanAudit = guild.audit.ban;
       guild.audit.ban = auditLog;
 
-      console.log('2');
-
       if (auditLog.target.id != member.id) return resolve(null);
-
-      console.log('3');
 
       if (lastBanAudit) {
         if (lastBanAudit.id == auditLog.id) return resolve(null);
       }
-
-      console.log('4');
 
       return resolve(auditLog);
     } catch { resolve(null); }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -21,7 +21,7 @@ module.exports = (client, guildMember) => {
 
       log.send(guild, { member: guildMember, executor: executor, reason: audit.reason }, log.Type.KICK);
     } catch { }
-  }, 500, guildMember)
+  }, process.env.delay, guildMember)
 }
 
 function checkAuditEntry(guild, member) {
