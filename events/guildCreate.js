93c0a9a7da191879837bf53c440ddb4ebadc5d5b
unknown <theoscar@protonmail.com>
2020-08-21 05:45:03 +0100

    test

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,10 +4,10 @@ const sql = require('../sql.js');
 module.exports = async (client, guild) => {
   try {
     guild.db = await sql.loadGuild(client, guild.id);
-    guild.infractions = await sql.loadInfractionCount(guild.id);
+    //guild.infractions = await sql.loadInfractionCount(guild.id);
     guild.ready = true;
 
-    console.log(guild.infractions);
+    //console.log(guild.infractions);
 
     functions.loadGuildHooks(client, guild);
   } catch { }
