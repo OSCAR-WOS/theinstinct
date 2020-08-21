86964548ef4a7e9e20dacdcc6559fd5cd8ccfdf1
unknown <theoscar@protonmail.com>
2020-08-21 05:46:23 +0100

    test

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = async (client) => {
   for (let guild of client.guilds.cache.values()) {
     try {
       guild.db = await sql.loadGuild(client, guild.id);
-      guild.infractions = await sql.loadInfractionCount(guild.id);
+      guild.infractions = await sql.loadInfractionCount(guild.id).length;
       guild.ready = true;
 
       console.log(guild.infractions);
