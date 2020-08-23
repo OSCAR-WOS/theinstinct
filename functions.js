25c5e3011f66513e9fbd04ae68148eb077646a79
unknown <theoscar@protonmail.com>
2020-08-23 12:45:12 +0100

    test

--- functions.js
+++ functions.js
@@ -131,9 +131,9 @@ function resolveUserString(message, string, type) {
 
     let first = collection.first();
     try { await messageDelete(first, true);
-    } catch { }
+    } catch (e) { console.error(e); }
 
-    console.log(`1${first}`);
+    console.log(first);
 
     let pick = parseInt(first.content);
     if (isNaN(pick) || pick < 0 || pick > users.length - 1) { await sendMessage(message.channel, messageType.ERROR, { content: util.format(translatePhrase('target_invalid', message.guild ? message.guild.db.lang : process.env.lang), first.content, users.length - 1)}); return resolve(null); }
