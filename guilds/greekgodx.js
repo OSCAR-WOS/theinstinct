734805be58525c2decd8151f9a5a9bdc584e0e3e
unknown <theoscar@protonmail.com>
2020-08-23 07:38:48 +0100

    greekgodx: added check for img attachment

--- guilds/greekgodx.js
+++ guilds/greekgodx.js
@@ -4,6 +4,8 @@ let functions = require('../functions.js');
 client.on('message', async message => {
   if (message.channel.id == '746388677978095748') {
     if (message.attachments.size == 0) return functions.deleteMessage(message, true);
-    //let attachments = message.attachments;
+
+    let attachment = message.attachments.first();
+    if (!attachment.height) return functions.deleteMessage(message, true);
   }
 })
