05555238704394e38421697ddfc70dfe4d1a463a
unknown <theoscar@protonmail.com>
2020-08-23 07:26:44 +0100

    forgot async

--- guilds/greekgodx.js
+++ guilds/greekgodx.js
@@ -1,7 +1,7 @@
 let client = require('../index.js');
 let functions = require('../functions.js');
 
-client.on('message', message => {
+client.on('message', async message => {
   if (message.channel.id == '746388677978095748') {
     if (message.attachments.size == 0) return functions.deleteMessage(message, true);
     //let attachments = message.attachments;
