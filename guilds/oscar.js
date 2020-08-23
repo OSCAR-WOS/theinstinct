b0f3c0ac481da9d069c828bd60a7c496e5ba50af
unknown <theoscar@protonmail.com>
2020-08-23 11:21:15 +0100

    ignore bot messages

--- guilds/oscar.js
+++ guilds/oscar.js
@@ -2,6 +2,7 @@ let client = require('../index.js');
 let functions = require('../functions.js');
 
 client.on('message', async message => {
+  if (message.author.bot) return;
   if (!message.guild || message.guild.id != '677290032696131590') return;
   try {
     let user = await functions.resolveUser(message, message.content, true);
