d7bfd612285c2d4a48fe597d794fbad67ff12bd4
unknown <theoscar@protonmail.com>
2020-08-23 09:54:06 +0100

    added exception for admins for sfw channel greek

--- guilds/greekgodx.js
+++ guilds/greekgodx.js
@@ -3,6 +3,7 @@ let functions = require('../functions.js');
 
 client.on('message', async message => {
   if (message.channel.id == '746388677978095748') {
+    if (message.member.permissions.has('ADMINISTRATOR')) return;
     if (message.attachments.size == 0) return functions.deleteMessage(message, true);
 
     let attachment = message.attachments.first();
--- guilds/oscar.js
+++ guilds/oscar.js
@@ -3,9 +3,4 @@ let functions = require('../functions.js');
 
 client.on('message', async message => {
   if (!message.guild || message.guild.id != '677290032696131590') return;
-
-  if (message.content == 'test') {
-    try { await functions.sendMessage(message.channel, functions.messageType.SUCCESS, { content: 'test', footer: 'by 123' });
-    } catch (e) { console.error(e); }
-  }
 })
