5457ab750682c51f44fdb4cecd890f617efaad3b
unknown <theoscar@protonmail.com>
2020-08-23 17:35:29 +0100

    REMOVED ADMIN EXEMPT

--- guilds/greekgodx.js
+++ guilds/greekgodx.js
@@ -24,7 +24,7 @@ var regex = new RegExp(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z
 
 client.on('message', async message => {
   if (message.channel.id == '746388677978095748') {
-    if (message.member.permissions.has('ADMINISTRATOR')) return;
+    //if (message.member.permissions.has('ADMINISTRATOR')) return;
     if (!checksfw(message)) return functions.deleteMessage(message, true);
   }
 })
