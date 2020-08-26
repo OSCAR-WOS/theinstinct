7421ce8ffb16c93d4b86c45194b1bbe54adf83ed
unknown <theoscar@protonmail.com>
2020-08-26 11:02:24 +0100

    fixed issue with sql results

--- commands/reason.js
+++ commands/reason.js
@@ -18,7 +18,8 @@ module.exports = {
         if (isNaN(number) || number < 1 || number > message.guild.infractions - 1) return resolve(await functions.sendMessage(message.channel, functions.messageType.ERROR, { content: util.format(functions.translatePhrase('case_invalid', message.guild.db.lang), message.guild.infractions - 1)}));
 
         let query = await sql.findInfractions(message.guild.id, { id: number });
-        if (!query || !query.data.message) return resolve(await functions.sendMessage(message.channel. functions.messageType.ERROR, { content: util.format(functions.translatePhrase('case_notfound', message.guild.db.lang), number)}));
+        console.log(query);
+        if (!query || !query[0].data.message) return resolve(await functions.sendMessage(message.channel. functions.messageType.ERROR, { content: util.format(functions.translatePhrase('case_notfound', message.guild.db.lang), number)}));
 
         console.log(query);
 
