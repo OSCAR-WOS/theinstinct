27419a088f2b067499611a61a87c03a5fd4c6532
OSCAR-WOS <theoscar@protonmail.com>
2020-10-11 10:53:23 +0100

    re-add cert giver

--- site.js
+++ site.js
@@ -1,3 +1,5 @@
+const lcResponse = process.env.CERTBOT_RESPONSE;
+
 const express = require('express');
 var app = express();
 
@@ -5,19 +7,8 @@ app.get('/', (req, res) => {
     res.send('Test');
 })
 
-app.listen(process.env.PORT);
-
-/*
-
-const lcResponse = process.env.CERTBOT_RESPONSE;
-
-var app = express();
-
 app.get('/.well-known/acme-challenge/:content', (req, res) => {
     res.send(lcResponse);
 })
 
-app.listen(process.env.PORT, () => {
-    console.log('listening');
-})
-*/
+app.listen(process.env.PORT);
