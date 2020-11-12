a12f1ed426c7ee10bc16cfd7686247ecfcbd97f9
Oscar <theoscar@protonmail.com>
2020-11-12 00:54:15 +0000

    re-wrote memberRemove and remove infraction headers

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,15 +1,10 @@
 const functions = require('../helpers/functions.js');
-const infraction = require('../helpers/infraction.js');
 const log = require('../helpers/log.js');
 
 module.exports = (client, member) => {
   setTimeout(async (member) => {
     if (member.banned) return;
-    let audit;
-
-    if (member.guild.me.permissions.has('VIEW_AUDIT_LOG')) {
-      audit = await checkKickEntry(member.guild, member);
-    }
+    const audit = await checkKickEntry(member.guild, member);
 
     if (!audit) {
       try {
@@ -21,29 +16,22 @@ module.exports = (client, member) => {
     try {
       await log.send(member.guild, log.Type.KICK, {member, executor, reason: audit.reason});
     } catch { }
-
-    if (executor.user.bot) return;
-    try {
-      await infraction.send(member.guild, infraction.Type.KICK, {member, executor, reason: audit.reason});
-    } catch { }
   }, process.env.delay, member);
 };
 
-checkKickEntry = (guild, member) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const auditLog = await functions.fetchAuditLog(guild, 'MEMBER_KICK');
-      if (!auditLog) return resolve();
+checkKickEntry = async (guild, member) => {
+  try {
+    const auditLog = await functions.fetchAuditLog(guild, 'MEMBER_KICK');
+    if (!auditLog) return;
 
-      const lastKickAudit = guild.audit.kick;
-      guild.audit.kick = auditLog;
+    const lastKickAudit = guild.audit.kick;
+    guild.audit.kick = auditLog;
 
-      if (auditLog.target.id !== member.id) return resolve();
-      if (lastKickAudit && lastKickAudit.id === auditLog.id) return resolve();
+    if (auditLog.target.id !== member.id) return;
+    if (lastKickAudit && lastKickAudit.id === auditLog.id) return;
 
-      return resolve(auditLog);
-    } catch {
-      resolve();
-    }
-  });
+    return auditLog;
+  } catch {
+    return;
+  }
 };
