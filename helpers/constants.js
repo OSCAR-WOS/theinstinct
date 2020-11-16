87086560118b1fc527598cd1697a740da5724821
OSCAR-WOS <theoscar@protonmail.com>
2020-11-16 19:56:44 +0000

    Add constants.js

---
+++ helpers/constants.js
@@ -0,0 +1,49 @@
+exports.Resolve = {
+  ALL: 'all',
+  GUILD: 'guild',
+};
+
+exports.LogSetting = {
+  SIMPLE: 'simple',
+  DETAILED: 'detailed',
+};
+
+exports.AttachmentError = {
+  SIZE: 'size',
+  UNSUPPORTED: 'unsupported',
+};
+
+exports.Log = {
+  MESSAGE_DELETE: 'message_delete',
+  MESSAGE_UPDATE: 'message_update',
+  MESSAGE_BULK_DELETE: 'message_bulk_delete',
+  JOIN: 'join',
+  LEAVE: 'leave',
+  KICK: 'kick',
+  BAN: 'ban',
+  UNBAN: 'unban',
+  ROLE_ADD: 'role_add',
+  ROLE_REMOVE: 'role_remove',
+  MUTE_ADD: 'mute_add',
+  PUNISH_ADD: 'punish_add',
+  GAG_ADD: 'gag_add',
+  MUTE_REMOVE: 'mute_remove',
+  PUNISH_REMOVE: 'punish_remove',
+  GAG_REMOVE: 'gag_remove',
+  USERNAME_UPDATE: 'username_update',
+  NICKNAME_UPDATE: 'nickname_update',
+};
+
+exports.Message = {
+  NORMAL: 'type_normal',
+  CODE: 'type_code',
+  SUCCESS: 'type_success',
+  ERROR: 'type_error',
+  EMBED: 'type_embed',
+  USAGE: 'type_usage',
+};
+
+exports.Webhook = {
+  FILES: 'Files',
+  LOGS: 'Logs',
+};
