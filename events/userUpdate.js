8999984405b7d9d4562517daea6ba20ca22d2952
OSCAR-WOS <theoscar@protonmail.com>
2020-12-08 20:46:25 +0000

    Added failsafe for userupdate

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -3,7 +3,7 @@ const log = require('../helpers/log.js');
 
 module.exports = (client, oldUser, newUser) => {
   if (oldUser.tag === newUser.tag) return;
-  const guilds = client.guilds.cache.filter((guild) => guild.ready && guild.member(newUser));
+  const guilds = client.guilds.cache.filter((guild) => guild && guild.ready && guild.member(newUser));
 
   guilds.forEach(async (guild) => {
     const member = guild.member(newUser);
