67e92c7dd1b1c43685d030abfa0f4f43dec5e1af
OSCAR-WOS <theoscar@protonmail.com>
2020-12-09 18:20:34 +0000

    Check guild

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,15 +1,15 @@
 const constants = require('../helpers/constants.js');
 const log = require('../helpers/log.js');
 
-module.exports = (client, oldUser, newUser) => {
+module.exports = async (client, oldUser, newUser) => {
   if (oldUser.tag === newUser.tag) return;
 
-  client.guilds.cache.forEach(async (guild) => {
+  for (const guild of client.guilds.cache) {
     const member = guild.member(newUser);
     if (!member) return;
 
     try {
       await log.send(guild, constants.Log.USERNAME_UPDATE, {oldUser, member});
     } catch { }
-  });
+  }
 };
