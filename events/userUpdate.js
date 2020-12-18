68667c22745f4c947cc22749203e56d6fe2c1ebc
OSCAR-WOS <theoscar@protonmail.com>
2020-12-18 05:45:16 +0000

    Fixed userUpdate guild values

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -4,7 +4,7 @@ const log = require('../helpers/log.js');
 module.exports = async (client, oldUser, newUser) => {
   if (oldUser.tag === newUser.tag) return;
 
-  for (const guild of client.guilds.cache) {
+  for (const guild of client.guilds.cache.values()) {
     const member = guild.members.resolve(newUser);
     if (!member) return;
 
